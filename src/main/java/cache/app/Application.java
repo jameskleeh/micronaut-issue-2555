dde47ed1c1148c478da331c6b0d61c836e10f0b1
jameskleeh <james.kleeh@gmail.com>
2019-12-20 14:36:59 -0500

    Sample app

---
+++ src/main/java/cache/app/Application.java
@@ -0,0 +1,16 @@
+package cache.app;
+
+import io.micronaut.context.ApplicationContext;
+import io.micronaut.runtime.Micronaut;
+import jar.app.CalculatorService;
+
+public class Application {
+
+    public static void main(String[] args) {
+        ApplicationContext ctx = Micronaut.run(Application.class);
+        CalculatorService calculatorService = ctx.getBean(CalculatorService.class);
+        calculatorService.addOne(1);
+        calculatorService.addOne(1);
+        calculatorService.addOne(1);
+    }
+}
